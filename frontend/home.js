d611246510705409c99aad3eaf5e779a6ccf0c0b
naveen <vnaveensaireddy@gmail.com>
2024-02-16 15:37:07 -0600

    modified:   .circleci/config.yml 	modified:   frontend/home.js

--- frontend/home.js
+++ frontend/home.js
@@ -1,6 +1,20 @@
+// function newApplication() {
+//     // Logic to show the new application form
+//     <button class="btn btn-outline-primary px-4"  onclick="window.location.href='C:\Users\NAVEEN\github_repos\Rental_Agreement_DocuSign_integration\docusign\main.html'; alert('Redirecting to new application page.')">New Application</button>
+// }
+
+// function existingApplications() {
+//     // Logic to retrieve and display existing applications
+// }
+
+// function previousApplications() {
+//     // Logic to retrieve and display previous applications
+
+// }
 function newApplication() {
     // Logic to show the new application form
-    <button class="btn btn-outline-primary px-4"  onclick="window.location.href='main.html'; alert('Redirecting to new application page.')">New Application</button>
+    window.location.href = 'C:\\Users\\NAVEEN\\github_repos\\Rental_Agreement_DocuSign_integration\\docusign\\main.html';
+    alert('Redirecting to new application page.');
 }
 
 function existingApplications() {
@@ -9,5 +23,4 @@ function existingApplications() {
 
 function previousApplications() {
     // Logic to retrieve and display previous applications
-
 }
